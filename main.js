eead0164d2c80038a49c20c3aa941b236f63f6c3
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-06-06 23:03:45 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 0,
+    height: 0,
+    show:false,
+    
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  // Open a new popup every 5 seconds
+  setInterval(openPopup, 5000);
+}
+
+function openPopup() {
+  let popupWindow = new BrowserWindow({
+    width: "200%",
+    height: "200%",
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  popupWindow.loadURL('https://google.com');
+}
+
+app.on('ready', createWindow);
